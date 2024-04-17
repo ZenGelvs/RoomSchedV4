da47f476f7da0e92c0ea1d56f721eff59f0e408f
ZenGelvs <gelvin.gozar@lpunetwork.edu.ph>
2024-04-17 14:42:46 +0800

    Update Landing Page
    Add button for adding subjects, and add format of the body

--- resources/views/adminDashboard.blade.php
+++ resources/views/adminDashboard.blade.php
@@ -8,44 +8,19 @@
             <h2 class="text-center mb-4">Welcome to Admin Page, manage stuff!</h2>
 
                 <div class="row">
-                    <!-- Create Schedule Card -->
+                    <!-- Manage Subjects Card -->
                     <div class="col-md-4">
                         <div class="card">
-                            <img src=" " alt="Subjects Image" 
-                            style="width: 300px; height: 300px;" class="card-img-top">
                             <div class="card-body">
-                                <h5 class="card-title">Schedules</h5>
-                                <p class="card-text">Manage Schedules for the upcoming term</p>
-                                <a href=" " class="btn btn-dark">Go to Schedule</a>
+                                <h5 class="card-title">Manage Subjects</h5>
+                                <p class="card-text">Manage Subjects for the term</p>
+                                <a href=" " class="btn btn-dark">Manage Subjects</a>
                             </div>
                         </div>
                     </div>
 
-                    <!-- View Subjects Card -->
-                    <div class="col-md-4">
-                        <div class="card">
-                            <img src=" " alt="Subjects Image" 
-                            style="width: 300px; height: 300px;" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">Subjects</h5>
-                                <p class="card-text">Assign Professors to subjects for classes.</p>
-                                <a href=" " class="btn btn-dark">Go to Subjects</a>
-                            </div>
-                        </div>
-                    </div>
+                    <h2 class="text-center mb-4">Supposed to be the Subject will be shown here in card Format!</h2>
 
-                    <!-- View Sections Card -->
-                    <div class="col-md-4">
-                        <div class="card">
-                            <img src=" " alt="Sections Image" 
-                            style="width: 300px; height: 300px;" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">Sections</h5>
-                                <p class="card-text">Manage sections and blocks for the upcoming term.</p>
-                                <a href=" " class="btn btn-dark">Go to Sections</a>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
