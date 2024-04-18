57196b48c207a8f2fa15115b77a5ca50d221d96c
ZenGelvs <gelvin.gozar@lpunetwork.edu.ph>
2024-04-19 00:52:22 +0800

    Notification
    Added notification success when adding new subjects

--- resources/views/admin/manage_subjects.blade.php
+++ resources/views/admin/manage_subjects.blade.php
@@ -4,6 +4,14 @@
 
 @section('content')
     <div class="container mt-4">
+        @if(session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card mb-4">
