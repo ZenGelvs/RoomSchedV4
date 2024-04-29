150c72fd186e9d11457d79ebb9ee69cf17fb9a28
ZenGelvs <gelvin.gozar@lpunetwork.edu.ph>
2024-04-29 18:58:23 +0800

    Add room table and populate with data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProgramSeeder::class,
             FacultySeeder::class,
-
+            RoomSeeder::class,
         ]);
     }
 }
